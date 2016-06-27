d857d5efc7d8965a0e341b165a6b5f812370e8d0
colinmorita <cmorita23@punahou.edu>
2016-06-27 11:55:38 -1000

    I made a list

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var arr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+var count = 0;
+
+function cycle(array/*parameter*/) {
+	
+	console.log(array[count];
+	count++ // or count += 1; or count = count +
+
+};
+
+var list = function () {
+	cycle(arr);
+	
+}
